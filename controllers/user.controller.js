cfa7e867c35ea0f1d87ac811f5cc4031580ec7be
Tad-z <david.akintade1000@gmail.com>
2023-09-20 23:00:53 +0100

    updates based on review

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,25 +1,25 @@
 const httpStatus = require('http-status');
 const Asyncly = require('../utils/Asyncly');
 const { authService, userService } = require('../services');
+const ApiError = require('../utils/ApiError');
 
 const getUserById = Asyncly(async (req, res, next) => {
     const { id } = req.params;
-    try {
-        const user = await userService.getPersonById({ id })
+    const user = await userService.getPersonById({ id })
+    if (user) {
         return res.status(httpStatus.OK).json({ user })
-    } catch (error) {
-        return next(error);
     }
+    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
+
 })
 
 const getUserByEmail = Asyncly(async (req, res, next) => {
-    try{
-        const { email } = req.params
-        const user = await userService.getPersonByEmail({ email })
+    const { email } = req.params
+    const user = await userService.getPersonByEmail({ email })
+    if (user) {
         return res.status(httpStatus.OK).json({ user })
-    } catch(error) {
-        return next(error)
-    } 
+    }
+    throw new ApiError(httpStatus.NOT_FOUND, 'User not found');
 })
 
 module.exports = { getUserById, getUserByEmail }
