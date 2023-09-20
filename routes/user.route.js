7dcf69f5c85f89510d3f4ca160892fd5f431ee68
Tad-z <david.akintade1000@gmail.com>
2023-09-20 13:51:40 +0100

    get user by id or email

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { updateUser, getUserById, getUserByEmail } = require("../controllers/user.controller");
+const { getUserById, getUserByEmail } = require("../controllers/user.controller");
 
 
 const router = express.Router();
-router.put("/:id", updateUser)
+
 router.get("/:id", getUserById)
 router.get("/:email", getUserByEmail)
 
