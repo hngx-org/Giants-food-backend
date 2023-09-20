f5ccb4d90b859cd33042ee897beaba286d576a84
homoakin619 <homohaykins619@gmail.com>
2023-09-20 18:08:13 +0100

    fetched single lunch

--- controllers/lunch.controller.js
+++ controllers/lunch.controller.js
@@ -1,18 +1,32 @@
 const httpStatus = require('http-status');
 const Asyncly = require('../utils/Asyncly');
-const { authService, lunchService } = require('../services');
+const { authService } = require('../services');
+
+const { getLunchesForOrganization,getSingleLunch } = require('../services/lunch.service');
 
 const fetchLunchesForOrg = async (req, res) => {
-	const { getLunchesForOrganization } = lunchService;
+	
 
 	const orgId = null; // Organization ID will be gotten from access token when implemented
 	const allLunches = await getLunchesForOrganization(orgId);
+
 	res.status(200).json({
 		status_code: httpStatus.OK,
 		message: 'success',
 		data: allLunches,
 	});
 };
+
+const fetchSingleLunch = async (request,response) => {
+	const lunchId = req.params.id
+	const singleLunch = getSingleLunch(lunchId)
+	
+	response.status(200).json({
+		status_code: httpStatus.OK,
+		message: "Lunch fetched Successfully",
+		data: singleLunch
+	})
+}
 
 module.exports = {
 	fetchLunchesForOrg,
