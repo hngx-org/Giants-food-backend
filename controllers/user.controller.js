496c91d6d5f93b05761a8eaecbb835fcbd49ff13
tobey0x <omoniyiadejengbe@gmail.com>
2023-09-20 17:13:50 +0100

    set-up the getUsersByOrgID in the user.controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,31 @@
+const express = require("express")
 const httpStatus = require('http-status');
 const Asyncly = require('../utils/Asyncly');
-const { authService } = require('../services');
+const { authService, userService } = require('../services');
+
+
+
+const getUsersByOrgID = async (req, res, next) => {
+  try {
+    const org_id = req.params.org_id;
+
+  const users = await userService.getPersonsByOrgID({ id: org_id });
+
+  if (!users) {
+    return res.status(404).json({
+      message: "No User Found From The Organization",
+    });
+  }
+
+  res.status(200).json({
+    message: "Here are the Users from the organization",
+    data: users,
+  })
+  } catch (error) {
+    next(error);
+  };
+};
+
+module.exports = {
+  getUsersByOrgID,
+}
