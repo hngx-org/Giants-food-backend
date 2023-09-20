e169f2214cffb9cb8609ed36fe0ceec6fa8743c9
Edet Asuquo <eddy1759@gmail.com>
2023-09-20 16:44:44 +0100

    fix: linting error fixed

--- controllers/organization.controller.js
+++ controllers/organization.controller.js
@@ -4,52 +4,50 @@ const ApiError = require('../utils/ApiError');
 const { authService } = require('../services');
 
 const acceptInvite = Asyncly(async (req, res) => {
-    try {
+	try {
+		const inviteToken = req.query.inviteToken;
 
-        const inviteToken = req.query.inviteToken;
+		if (!inviteToken) {
+			throw new ApiError(httpStatus.BAD_REQUEST, 'Invite token not found');
+		}
+		const { first_name, last_name, email, phone_number, password_hash } =
+			req.body;
 
-        if (!inviteToken) {
-            throw new ApiError(httpStatus.BAD_REQUEST, 'Invite token not found');
-        }
-        const { 
-            first_name, 
-            last_name, 
-            email, 
-            phone_number,
-            password_hash 
-        } = req.body;
+		if (
+			!first_name ||
+			!last_name ||
+			!email ||
+			!phone_number ||
+			!password_hash
+		) {
+			throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid details');
+		}
 
-        if ( 
-            !first_name || 
-            !last_name ||
-            !email ||
-            !phone_number ||
-            !password_hash
-        )   {
-                throw new ApiError(httpStatus.BAD_REQUEST, 'Invalid details');
-            }
+		const userBody = {
+			first_name,
+			last_name,
+			email,
+			phone_number,
+			password_hash,
+		};
 
-        const userBody = { first_name, last_name, email, phone_number, password_hash };
-
-        
-    const data = organizationService.handleOrganizationOnboarding(inviteToken, userBody);
-
-    if (!data) {
-        throw new ApiError(httpStatus.NOT_FOUND, 'not found');
-    }
-
-    res.status(httpStatus.CREATED).json({ 
-        status: true,
-        message: data.message,
-        user: data.user
-     })
-    }catch (error) {
-        throw new ApiError(
-			httpStatus.INTERNAL_SERVER_ERROR,
-            error
+		const data = organizationService.handleOrganizationOnboarding(
+			inviteToken,
+			userBody,
 		);
-    }
-    
+
+		if (!data) {
+			throw new ApiError(httpStatus.NOT_FOUND, 'not found');
+		}
+
+		res.status(httpStatus.CREATED).json({
+			status: true,
+			message: data.message,
+			user: data.user,
+		});
+	} catch (error) {
+		throw new ApiError(httpStatus.INTERNAL_SERVER_ERROR, error);
+	}
 });
 
 module.exports = { acceptInvite };
