5dab83339d69c57d9da4c7982d3388f8d9fbead7
Nicholas <nicholasikiroma@gmail.com>
2023-09-20 10:11:58 +0100

    feature: get all lunches for organisation

--- routes/lunch.route.js
+++ routes/lunch.route.js
@@ -1,1 +1,8 @@
-const express = require("express");
+const express = require('express');
+const lunchController = require('../controllers/lunch.controller');
+
+const router = express.Router();
+
+router.get('/', lunchController.fetchLunchesForOrg);
+
+module.exports = router;
--- services/launch.service.js
+++ services/launch.service.js
@@ -1,3 +1,33 @@
 const httpStatus = require('http-status');
 const { dB } = require('../models');
+const Organization = dB.organizations;
 const ApiError = require('../utils/ApiError');
+
+/**
+ * Fetches all lunches for a specific organization.
+ * @param {number} organizationId - The ID of the organization.
+ * @returns {Promise<Array>} - A promise that resolves to an array of lunches.
+ * @throws {ApiError} - If the organization is not found or an error occurs.
+ */
+async function getLunchesForOrganization(organizationId) {
+	try {
+		const organization = await Organization.findByPk(organizationId);
+
+		if (!organization) {
+			throw new ApiError(httpStatus.NOT_FOUND, 'Organization not found', true);
+		} else {
+			const lunches = await organization.getLunches();
+			return lunches;
+		}
+	} catch (err) {
+		throw new ApiError(
+			httpStatus.INTERNAL_SERVER_ERROR,
+			'Failed to fetch lunches for the organization',
+			true,
+		);
+	}
+}
+
+module.exports = {
+	getLunchesForOrganization,
+};
