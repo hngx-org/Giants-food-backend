238b1c389ce0eef7909ae91626852262f53c50f7
Sxamoecode <samuelayandare@gmail.com>
2023-09-20 20:03:18 +0100

    In process

--- controllers/invite.controller.js
+++ controllers/invite.controller.js
@@ -3,13 +3,18 @@ const Asyncly = require('../utils/Asyncly');
 const {generateToken} = require('../services/token.service.js')
 
 exports.invite = Asyncly(async (req, res) => {
-    const { email } = req.body;
-    if (!email) {
-        return res.status(400).json({
-            Msg: 'Invalid email address'
-        });
+    try {
+        const { email } = req.body;
+        if (!email) {
+            return res.status(400).json({
+                Msg: 'Invalid email address'
+            });
+        }
+        token = generateToken();
+    } catch (error) {
+        console.log(error.message);
+        return res.statusCode(500).send('Server error');
     }
 
-    token = generateToken();
 })
 // In process!!!
