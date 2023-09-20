93f180206db6558fe76a6b1f8bb71e6b5696a004
StephanieMfon <108840244+StephanieMfon@users.noreply.github.com>
2023-09-20 20:45:04 +0100

    further corrections

--- controllers/organization.controller.js
+++ controllers/organization.controller.js
@@ -11,15 +11,6 @@ const Asyncly = require('../utils/Asyncly');
 
 //Create New Organization
 const createOrganizationController = Asyncly(async (req, res, next) => {
-	if (error)
-		return new ApiError(
-			httpStatus.BAD_REQUEST,
-			error.details[0].message,
-			true,
-			'',
-			true,
-		);
-
 	const newOrganization = await createOrganizationService(req.body);
 	return res.status(httpStatus.CREATED).json({
 		status_code: httpStatus.CREATED,
