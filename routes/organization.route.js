df3c2cbba12e287f9d512e21ef7b4503fc50073e
pipthablack <emezuechinonso34@gmail.com>
2023-09-20 20:35:41 +0100

    created organization route

--- routes/organization.route.js
+++ routes/organization.route.js
@@ -1,1 +1,15 @@
 const express = require("express");
+const router = express.Router();
+const validate = require("../middlewares/validate");
+const { createOrganizationValidator } = require("../validation/organization.validation");
+const { organizationController } = require("../controllers");
+
+
+router.post (
+    "/",
+    validate(createOrganizationValidator.body),
+    organizationController.createOrganizationController,
+
+);
+
+
