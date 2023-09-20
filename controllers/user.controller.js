83e6a972abe2fadca0b2c0e30bac8af2583b8975
Tad-z <david.akintade1000@gmail.com>
2023-09-20 18:45:50 +0100

    get user by id or email

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -3,15 +3,23 @@ const Asyncly = require('../utils/Asyncly');
 const { authService, userService } = require('../services');
 
 const getUserById = Asyncly(async (req, res) => {
-    const { id } = req.params
-    const user = await userService.getPersonById({ id })
-    return res.status(httpStatus.OK).json({ user })
+    const { id } = req.params;
+    try {
+        const user = await userService.getPersonById({ id })
+        return res.status(httpStatus.OK).json({ user })
+    } catch (error) {
+        return next(error);
+    }
 })
 
 const getUserByEmail = Asyncly(async (req, res) => {
-    const { email } = req.params
-    const user = await userService.getPersonByEmail({ email })
-    return res.status(httpStatus.OK).json({ user })
+    try{
+        const { email } = req.params
+        const user = await userService.getPersonByEmail({ email })
+        return res.status(httpStatus.OK).json({ user })
+    } catch(error) {
+        return next(error)
+    } 
 })
 
 module.exports = { getUserById, getUserByEmail }
