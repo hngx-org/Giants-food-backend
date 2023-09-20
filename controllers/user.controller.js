db4e3ccfe6229f6d28673a33a855ba140172d40e
Tad-z <david.akintade1000@gmail.com>
2023-09-20 13:00:04 +0100

    setting up user routes and controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,3 +1,16 @@
 const httpStatus = require('http-status');
 const Asyncly = require('../utils/Asyncly');
 const { authService } = require('../services');
+const user = require('../models/user');
+
+exports.updateUser = Asyncly((req, res) => {
+
+})
+
+exports.getUserById = Asyncly((req, res) => {
+
+})
+
+exports.getUserByEmail = Asyncly((req, res) => {
+
+})
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,1 +1,10 @@
 const express = require("express");
+const { updateUser, getUserById, getUserByEmail } = require("../controllers/user.controller");
+
+
+const router = express.Router();
+router.put("/:id", updateUser)
+router.get("/:id", getUserById)
+router.get("/:email", getUserByEmail)
+
+module.exports = router
