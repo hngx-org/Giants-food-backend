7b415c05679794d484297c96fa485d1cc70cee8b
tobey0x <omoniyiadejengbe@gmail.com>
2023-09-20 17:14:46 +0100

    added the /:org_id to the user.route

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,1 +1,11 @@
 const express = require("express");
+const userController = require("../controllers/user.controller")
+const router = express.Router();
+
+router.get("/:org_id", userController.getUsersByOrgID);
+
+
+module.exports = {
+  router,
+}
+
