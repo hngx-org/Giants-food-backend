bf97c995865b3486886f80a2a2b4cb4a7809b6f1
StephanieMfon <108840244+StephanieMfon@users.noreply.github.com>
2023-09-20 19:27:49 +0100

    clean up

--- controllers/organization.controller.js
+++ controllers/organization.controller.js
@@ -9,32 +9,24 @@ const {
 const ApiError = require('../utils/ApiError');
 
 //Create New Organization
-const createOrganization = async (req, res) => {
-	try {
-		const { error } = createOrganizationValidator.body.validate(req.body);
-		if (error)
-			return new ApiError(
-				httpStatus.BAD_REQUEST,
-				error.details[0].message,
-				true,
-				'',
-				true,
-			);
-		else {
-			const newOrganization = await createOrganizationService(req.body);
-			console.log('NewOrt\n' + newOrganization);
-			return res.status(200).json({
-				status_code: httpStatus.OK,
-				message: 'success',
-				data: newOrganization,
-			});
-		}
-	} catch (error) {
-		res.status(httpStatus.BAD_REQUEST).json({
-			message: 'Failure',
-			error: error.message,
-		});
-	}
+const createOrganization = async (req, res, next) => {
+	const { error } = createOrganizationValidator.body.validate(req.body);
+	if (error)
+		return new ApiError(
+			httpStatus.BAD_REQUEST,
+			error.details[0].message,
+			true,
+			'',
+			true,
+		);
+
+	const newOrganization = await createOrganizationService(req.body);
+	console.log('NewOrt\n' + newOrganization);
+	return res.status(200).json({
+		status_code: httpStatus.OK,
+		message: 'success',
+		data: newOrganization,
+	});
 };
 
 module.exports = createOrganization;
