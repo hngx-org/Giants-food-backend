b2ddddecf90da656c3cbc35196bbb7d451a22eb7
gnobu <inyangubong17@gmail.com>
2023-09-20 13:31:04 +0100

    created create org branch

--- services/organization.service.js
+++ services/organization.service.js
@@ -1,3 +1,11 @@
 const httpStatus = require('http-status');
 const { dB } = require('../models');
 const ApiError = require('../utils/ApiError');
+
+/**
+ * Generate token
+ * @param {}
+ * @returns {}
+ */
+const createOrganization = (userId, expires, type, secret = config.jwt.secret) => {
+};
