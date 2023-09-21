9b8db81c74f2d9a42e3bbe5a6e23da315af1a4ed
nonso-uj <nonso.udonne@gmail.com>
2023-09-21 17:32:05 +0100

    added functionality to send free lunch

--- routes/lunch.route.js
+++ routes/lunch.route.js
@@ -1,8 +1,13 @@
 const express = require("express")
 const router = express.Router()
+const { verifyToken } = require('../middlewares/verify')
 const { giftLunch, redeemLunch } = require('../controllers/lunch.controller')
 
-router.post('/lunch', giftLunch);
+router.get('', (req, res) => {
+    res.json({hi: "there"})
+})
+router.post('', verifyToken, giftLunch);
 router.put('/redeem/:id', redeemLunch);
+
 
 module.exports = router;
--- services/lunch.service.js
+++ services/lunch.service.js
@@ -3,13 +3,13 @@ const { dB } = require('../models');
 const ApiError = require('../utils/ApiError');
 
 
-const giftLunch = async (lunchBody) => {
+const createLunch = async (lunchBody) => {
+    console.log(lunchBody)
     const lunch = await dB.lunches.create(lunchBody)
-
     if(!lunch){
         throw new ApiError(httpStatus.BAD_GATEWAY, 'Somethings wrong, Check your input and try again');
     }
     return lunch
 }
 
-module.exports = { giftLunch }
+module.exports = { createLunch }
