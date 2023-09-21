4203c4e73f9e4fc0fec8023c660b7ec06dcd8721
nonso-uj <nonso.udonne@gmail.com>
2023-09-21 21:32:57 +0100

    fixed lunch creation

--- routes/lunch.route.js
+++ routes/lunch.route.js
@@ -3,9 +3,6 @@ const router = express.Router()
 const { verifyToken } = require('../middlewares/verify')
 const { giftLunch, redeemLunch } = require('../controllers/lunch.controller')
 
-router.get('', (req, res) => {
-    res.json({hi: "there"})
-})
 router.post('', verifyToken, giftLunch);
 router.put('/redeem/:id', redeemLunch);
 
