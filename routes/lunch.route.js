bf9f2452386def0e885b3b31126c8981c1722e33
homoakin619 <homohaykins619@gmail.com>
2023-09-21 09:53:20 +0100

    added endpoint to route

--- routes/lunch.route.js
+++ routes/lunch.route.js
@@ -4,9 +4,11 @@ const {
 	giftLunch,
 	redeemLunch,
 	fetchLunchesForOrg,
+	fetchSingleLunch
 } = require('../controllers/lunch.controller');
 
 router.post('/gift', giftLunch);
+router.get('/lunch/:id',fetchSingleLunch);
 router.put('/redeem/:id', redeemLunch);
 router.get('/', fetchLunchesForOrg);
 
