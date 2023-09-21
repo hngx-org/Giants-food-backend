b8ef2b3f35882e130ba2e002ed6f56ee1983f305
Sxamoecode <samuelayandare@gmail.com>
2023-09-21 08:01:24 +0100

    In process

--- controllers/invite.controller.js
+++ controllers/invite.controller.js
@@ -1,6 +1,6 @@
 const organization_iv = require('../models/organizationInvite.js');
 const Asyncly = require('../utils/Asyncly');
-const { generateToken } = require('../services/token.service.js');
+const { generateInviteToken } = require('../services/token.service.js');
 
 
 exports.invite = Asyncly(async (req, res) => {
@@ -11,7 +11,7 @@ exports.invite = Asyncly(async (req, res) => {
                 Msg: 'Invalid email address'
             });
         }
-        token = generateToken();
+        token = generateInviteToken();
     } catch (error) {
         console.log(error.message);
         return res.statusCode(500).send('Server error');
@@ -19,3 +19,19 @@ exports.invite = Asyncly(async (req, res) => {
 
 })
 // In process!!!
+/**
+ POST
+/api/organization/invite
+Description: this endpoint handles creating an invite which will be sent to the email of the user that should be invited
+A token is generated and the id of that invite, which is what is populated in the organization-invite.
+The token will contain the organization_id for us to know what organization is creating the invite
+
+
+Body{
+email
+}
+
+RESPONSE
+{ "message": "success", }
+
+ */
