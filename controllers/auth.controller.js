bad3f2d0a5ef050835573938a5f7e8206857c412
gnobu <inyangubong17@gmail.com>
2023-09-24 09:59:01 +0100

    added org data to auth response

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,22 +1,30 @@
 const httpStatus = require('http-status');
 const Asyncly = require('../utils/Asyncly');
-const { authService, userService } = require('../services');
+const { authService, organizationService } = require('../services');
 
 const login = Asyncly(async (req, res) => {
 	const user = await authService.login(req.body);
+	const organization = await organizationService.findOrganization(user.user.org_id)
+	if (organization) {
+		user.user.organization = organization
+	}
 	res.status(httpStatus.OK).send(user);
 });
 
 
 const signup = Asyncly(async (req, res) => {
 	const userDetail = await authService.signup(req.body);
+	const organization = await organizationService.findOrganization(user.user.org_id)
+	if (organization) {
+		user.user.organization = organization
+	}
 	res.status(httpStatus.CREATED).send(userDetail);
 });
 
 
 const forgotPassword = Asyncly(async (req, res) => {
 	const resetPasswordToken = await authService.forgotPassword(req.body);
-	res.status(httpStatus.OK).send({resetPasswordToken});
+	res.status(httpStatus.OK).send({ resetPasswordToken });
 });
 
 const resetPassword = Asyncly(async (req, res) => {
